f94ba08ba7c2d9b9e4f902b2b5360d43577b1d42
alphaa1232 <115874629+alphaa1232@users.noreply.github.com>
2023-11-26 16:03:26 +0530

    first comm

---
+++ hello.py
@@ -0,0 +1,55 @@
+from flask import Flask,jsonify, request
+import sys
+print(sys.path)
+
+app = Flask(__name__)
+
+from CASHMAN.model.expense import Expense, ExpenseSchema
+from CASHMAN.model.income import Income, IncomeSchema
+from CASHMAN.model.transaction_type import TransactionType
+
+transactions = [
+    Income('Salary', 5000),
+    Income('Dividends', 200),
+    Expense('pizza', 50),
+    Expense('Rock Concert', 100)
+]
+
+@app.route("/")
+def hello_world():
+    return "Hello, World!"
+
+@app.route('/incomes')
+def get_incomes():
+    schema = IncomeSchema(many=True)
+    incomes = schema.dump(
+        filter(lambda t: t.type == TransactionType.INCOME, transactions)
+    )
+    return jsonify(incomes)
+
+
+@app.route('/incomes', methods=['POST'])
+def add_income():
+    income = IncomeSchema().load(request.get_json())
+    transactions.append(income)
+    return "", 204
+
+
+@app.route('/expenses')
+def get_expenses():
+    schema = ExpenseSchema(many=True)
+    expenses = schema.dump(
+        filter(lambda t: t.type == TransactionType.EXPENSE, transactions)
+    )
+    return jsonify(expenses)
+
+
+@app.route('/expenses', methods=['POST'])
+def add_expense():
+    expense = ExpenseSchema().load(request.get_json())
+    transactions.append(expense)
+    return "", 204
+
+
+if __name__ == "__main__":
+    app.run()
